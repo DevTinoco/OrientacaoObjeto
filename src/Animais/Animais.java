7ef15b865002d1192133c9129ca4a12d3ed409e2
Valdinei Tinoco <valdinei_ct@hotmail.com>
2023-11-13 22:56:13 -0300

    Animais

---
+++ src/Animais/Animais.java
@@ -0,0 +1,25 @@
+package Animais;
+
+public class Animais {
+
+	public static void main(String[] args) {
+
+		//Objeto, variavel de referencia
+		
+		Cachorro cachorro1 = new Cachorro();
+		
+		cachorro1.nome = "Puho";
+		cachorro1.cor = "Black";
+		cachorro1.altura = 35;
+		cachorro1.peso = 5.5;
+		cachorro1.tamanhoDoRabo = 5;
+		
+		System.out.println(cachorro1);
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/Animais/Cachorro.java
@@ -0,0 +1,15 @@
+package Animais;
+
+public class Cachorro {
+   //Classe:
+	
+	
+	public String nome;
+	public String cor;
+	public int altura;
+	public double peso;
+	public int tamanhoDoRabo;
+	
+	
+	
+}
